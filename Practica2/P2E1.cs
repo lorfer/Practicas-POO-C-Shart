95ca28266ce5aa97101fc0c626efadbb3f9445fb
Lorfer <fernandogarciaolivo@gmail.com>
2018-07-20 03:31:26 +0200

    Ejercicio 1 practica 2

--- Practica2/P2E1.cs
+++ Practica2/P2E1.cs
@@ -1,1 +1,24 @@
 //P2E1.cs
+
+/*
+    1) Reciba un numero de mes y muestre en pantalla el nombre del mes
+ */
+
+
+ using System;
+class program
+{
+	public static void Main(string[] args)
+	{
+       int mes = 1;
+       string[] meses = new string[13]  { " ","Enero", "Febrero", "Marzo","Abril", "Mayo", "Junio","julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+		 
+         System.Console.WriteLine("\t\n*Introduce un mes en numero ->");
+         mes = Convert.ToInt32(Console.ReadLine());
+
+             System.Console.WriteLine("El numero "+ mes + " Corresponde a: " + meses[mes]);     
+         
+        
+        Console.ReadKey();
+	}
+}
